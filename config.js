d50361eda4808f42f324177f137ad4e40306b8ff
till busch <till@bux.at>
2013-05-17 11:57:13 +0200

    rename limitedcreative to limited-creative

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 this.plugins = [
   { name: "worldedit",          lfile: "WorldEdit.jar" },
   { name: "worldguard",         lfile: "WorldGuard.jar" },
-  { name: "limitedcreative",    lfile: "LimitedCreative.jar" },
+  { name: "limited-creative",    lfile: "LimitedCreative.jar" },
 ];
 this.bukkitdir = "craftbukkit";
